f693d13fe13950bdbbee9a348a1a0389e3810f62
Diogo Gaspar <diogo.marques.gaspar@tecnico.ulisboa.pt>
2023-05-13 23:21:47 +0100

    Change sizeof_node to unary_operation

--- ast/sizeof_node.h
+++ ast/sizeof_node.h
@@ -2,22 +2,19 @@
 #define __MML_AST_SIZEOF_NODE_H__
 
 #include <cdk/ast/expression_node.h>
+#include <cdk/ast/unary_operation_node.h>
 
 namespace mml {
 
 /**
  * Class for describing sizeof nodes.
  */
-class sizeof_node : public cdk::expression_node {
-  cdk::expression_node *_expression;
+class sizeof_node : public cdk::unary_operation_node {
+public:
+  inline sizeof_node(int lineno, cdk::expression_node *argument)
+      : cdk::unary_operation_node(lineno, argument) {}
 
 public:
-  inline sizeof_node(int lineno, cdk::expression_node *expression)
-      : cdk::expression_node(lineno), _expression(expression) {}
-
-public:
-  inline cdk::expression_node *expression() { return _expression; }
-
   void accept(basic_ast_visitor *sp, int level) {
     sp->do_sizeof_node(this, level);
   }
