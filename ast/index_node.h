2dc2455ce771ff2bfd01cc808c82ec725a709324
Diogo Gaspar <diogo.marques.gaspar@tecnico.ulisboa.pt>
2023-05-14 10:55:53 +0100

    Move getters in index_node below the constructor

--- ast/index_node.h
+++ ast/index_node.h
@@ -13,15 +13,15 @@ class index_node : public cdk::lvalue_node {
   cdk::expression_node *_base, *_index;
 
 public:
-  inline cdk::expression_node *base() { return _base; }
-
-  inline cdk::expression_node *index() { return _index; }
-
   inline index_node(int lineno, cdk::expression_node *base,
                     cdk::expression_node *index)
       : cdk::lvalue_node(lineno), _base(base), _index(index) {}
 
 public:
+  inline cdk::expression_node *base() { return _base; }
+
+  inline cdk::expression_node *index() { return _index; }
+
   void accept(basic_ast_visitor *sp, int level) {
     sp->do_index_node(this, level);
   }
