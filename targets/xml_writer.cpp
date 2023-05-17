61bde8599630589c37ed702b4de6ecd7609a3c5b
Diogo Gaspar <diogo.marques.gaspar@tecnico.ulisboa.pt>
2023-05-17 16:03:40 +0100

    Implement next_node in XML Writer
    Closes #28

--- targets/xml_writer.cpp
+++ targets/xml_writer.cpp
@@ -201,8 +201,9 @@ void mml::xml_writer::do_stop_node(mml::stop_node *const node, int lvl) {
 }
 
 void mml::xml_writer::do_next_node(mml::next_node *const node, int lvl) {
-  // FIXME: currently empty in order to compile, isn't required for the first
-  // delivery
+  ASSERT_SAFE_EXPRESSIONS;
+  openTag(node, lvl);
+  closeTag(node, lvl);
 }
 
 void mml::xml_writer::do_return_node(mml::return_node *const node, int lvl) {
