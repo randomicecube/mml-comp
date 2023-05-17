8977f9dd190184020c28be58ead626430bac506a
Diogo Gaspar <diogo.marques.gaspar@tecnico.ulisboa.pt>
2023-05-17 16:04:27 +0100

    Implement return_node in XML Writer
    Closes #29

--- targets/xml_writer.cpp
+++ targets/xml_writer.cpp
@@ -207,8 +207,12 @@ void mml::xml_writer::do_next_node(mml::next_node *const node, int lvl) {
 }
 
 void mml::xml_writer::do_return_node(mml::return_node *const node, int lvl) {
-  // FIXME: currently empty in order to compile, isn't required for the first
-  // delivery
+  ASSERT_SAFE_EXPRESSIONS;
+  openTag(node, lvl);
+  openTag("retval", lvl + 2);
+  node->retval()->accept(this, lvl + 4);
+  closeTag("retval", lvl + 2);
+  closeTag(node, lvl);
 }
 
 //---------------------------------------------------------------------------
