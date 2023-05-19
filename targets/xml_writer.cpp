4df4cb11ce8acd8cb0ba05d3dbb838d8d58f68ac
Diogo Gaspar <diogo.marques.gaspar@tecnico.ulisboa.pt>
2023-05-19 14:33:15 +0100

    Use do_unary_operation in sizeof, identity and stack_alloc nodes in XML Writer

--- targets/xml_writer.cpp
+++ targets/xml_writer.cpp
@@ -297,18 +297,14 @@ void mml::xml_writer::do_input_node(mml::input_node *const node, int lvl) {
 void mml::xml_writer::do_identity_node(mml::identity_node *const node,
                                        int lvl) {
   // ASSERT_SAFE_EXPRESSIONS;
-  openTag(node, lvl);
-  node->argument()->accept(this, lvl + 2);
-  closeTag(node, lvl);
+  do_unary_expression(node, lvl);
 }
 
 //---------------------------------------------------------------------------
 
 void mml::xml_writer::do_sizeof_node(mml::sizeof_node *const node, int lvl) {
   // ASSERT_SAFE_EXPRESSIONS;
-  openTag(node, lvl);
-  node->argument()->accept(this, lvl + 2);
-  closeTag(node, lvl);
+  do_unary_expression(node, lvl);
 }
 
 //---------------------------------------------------------------------------
@@ -330,9 +326,7 @@ void mml::xml_writer::do_index_node(mml::index_node *const node, int lvl) {
 void mml::xml_writer::do_stack_alloc_node(mml::stack_alloc_node *const node,
                                           int lvl) {
   // ASSERT_SAFE_EXPRESSIONS;
-  openTag(node, lvl);
-  node->argument()->accept(this, lvl + 2);
-  closeTag(node, lvl);
+  do_unary_expression(node, lvl);
 }
 
 //---------------------------------------------------------------------------
